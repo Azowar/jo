be6561d85c830aa6d1dd081b9f3f9e6e1eb83fc1
Azowar <95975878+Azowar@users.noreply.github.com>
2023-01-04 08:25:18 -0800

    Update math-api.php

--- math-api.php
+++ math-api.php
@@ -1,21 +1,20 @@
 <?php
-// Extract the operator and operands from the URL
+
 $url = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $parts = explode('/', $url);
-// The first element will be empty, the second will be the operator,
-// and the third and fourth will be the operands
+
 $operator = $parts[1];
 $number1 = $parts[2];
 $number2 = $parts[3];
-// Validate the operands and convert them to floats
+
 $number1 = filter_var($number1, FILTER_VALIDATE_FLOAT);
 $number2 = filter_var($number2, FILTER_VALIDATE_FLOAT);
-// Initialize the result to 0
+
 $sum = 0;
 $result = [
     "result" => 0,
 ];
-// If the operands are valid numbers, perform the operation
+
 if (is_numeric($number1) && is_numeric($number2)) {
     switch ($operator) {
         case "add":
@@ -44,5 +43,5 @@ if (is_numeric($number1) && is_numeric($number2)) {
             break;
     }
 }
-// Return the result as a JSON object
+
 echo json_encode($result);
